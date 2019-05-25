0b9b6ce80f1b44276915221a838d0c3fa76c8dc5
Vytautas Šaltenis <vytas@rtfb.lt>
2019-05-25 14:37:53 +0300

    Move implementation of (rest) to stdlib
    Cleanup testdata/arr.lisp a bit.

--- stdlib/stdlib.lisp
+++ stdlib/stdlib.lisp
@@ -1,4 +1,12 @@
 (fn car (arr) (nth 0 arr))
 (fn first (arr) (car arr))
+(fn rest-impl (arr new-arr pos)
+  (cond
+    ((eq pos (len arr)) new-arr)
+    (t
+      (rest-impl arr (append new-arr (nth pos arr)) (+1 pos)))))
+(fn rest (arr)
+  (rest-impl arr (mk-array) 1))
+(fn cdr (arr) (rest arr))
 (fn +1 (arg) (+ arg 1))
 (fn 1+ (arg) (+1 arg))
--- testdata/arr.lisp
+++ testdata/arr.lisp
@@ -3,17 +3,4 @@
 (let i 1)
 (nth i (append arr 7))
 (nth 2 (append (mk-array) 3 (+ 2 3) 7))
-(fn car (arr) (nth 0 arr))
-(car arr)
-(fn +1 (arg) (+ arg 1))
-
-(fn rest-impl (arr new-arr pos)
-  (cond
-    ((eq pos (len arr)) new-arr)
-    (t
-      (rest-impl arr (append new-arr (nth pos arr)) (+1 pos)))))
-
-(fn rest (arr)
-  (rest-impl arr (mk-array) 1))
-
 (rest arr)
